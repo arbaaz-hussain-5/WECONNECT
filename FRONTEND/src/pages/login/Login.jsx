864efed29df4f398600542455e19691a8af128ea
Arbaaz <mdarbaazhussain666@gmail.com>
2025-07-26 12:02:33 +0530

    arb

--- FRONTEND/src/pages/login/Login.jsx
+++ FRONTEND/src/pages/login/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
 
         <button
           onClick={async () => {
-            await fetch(`/${import.meta.env.VITE_SERVER_URL}/login`, {
+            await fetch(`${import.meta.env.VITE_SERVER_URL}/login`, {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
