b51d59775a01f11cdbb6dad63772d25fa3afc28b
Arbaaz <mdarbaazhussain666@gmail.com>
2025-07-26 10:06:50 +0530

    t

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,12 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API requests starting with /api to localhost:3000
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
-        // Optional: rewrite /api to /
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
